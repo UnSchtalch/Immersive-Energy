a6a4ffa71feda749f47105155822f9bac8626194
UnSchtalch <shadow.93.04@gmail.com>
2021-06-12 16:43:31 +0300

    Cleanup of core class

--- src/main/java/crimson_twilight/immersive_energy/ImmersiveEnergy.java
+++ src/main/java/crimson_twilight/immersive_energy/ImmersiveEnergy.java
@@ -1,64 +1,111 @@
 package crimson_twilight.immersive_energy;
 
-import net.minecraft.block.BlockDispenser;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.dispenser.BehaviorProjectileDispense;
-import net.minecraft.dispenser.IPosition;
-import net.minecraft.entity.IProjectile;
-import net.minecraft.entity.projectile.EntityArrow;
+import crimson_twilight.immersive_energy.client.ClientProxy;
+import crimson_twilight.immersive_energy.common.config.IEnServerConfig;
+import crimson_twilight.immersive_energy.common.util.network.IEnPacketHandler;
 
-import blusunrize.immersiveengineering.common.IEContent;
+
+
 import crimson_twilight.immersive_energy.client.RenderGuiHandler;
 import crimson_twilight.immersive_energy.common.CommonProxy;
 import crimson_twilight.immersive_energy.common.IEnContent;
 import crimson_twilight.immersive_energy.common.compat.IEnCompatModule;
-import crimson_twilight.immersive_energy.common.entities.EntityIEnArrow;
-import crimson_twilight.immersive_energy.common.items.IEnArrowBase;
-import crimson_twilight.immersive_energy.common.world.IEnWorldGen;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
+
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import java.util.logging.Logger;
 
-@Mod(modid = ImmersiveEnergy.MODID, name = ImmersiveEnergy.MODNAME, version = ImmersiveEnergy.MODVERSION, dependencies = "required-after:immersiveengineering;after:sereneseasons", useMetadata = true)
+//@Mod(modid = ImmersiveEnergy.MODID, name = ImmersiveEnergy.MODNAME, version = ImmersiveEnergy.MODVERSION, dependencies = "required-after:immersiveengineering;after:sereneseasons", useMetadata = true)
 public class ImmersiveEnergy 
 {
 
     public static final String MODID = "immersive_energy";
     public static final String MODNAME = "Immersive Energy";
-    public static final String MODVERSION= "0.6.0";
+    public static final String MODVERSION= "0.7.0";
 
-	@Mod.Instance(MODID)
-	public static ImmersiveEnergy instance = new ImmersiveEnergy();
+    public static final ItemGroup creativeTab = new ItemGroup(MODID) {
+		@Override
+		public ItemStack createIcon() {
+			return new ItemStack(IEnContent.blockGenerators0);
+		}
+	};
 
-    @SidedProxy(clientSide = "crimson_twilight.immersive_energy.client.ClientProxy", serverSide = "crimson_twilight.immersive_energy.common.CommonProxy")
-    public static CommonProxy proxy;
-
+	public static ImmersiveEnergy INSTANCE = new ImmersiveEnergy();
+	public static CommonProxy proxy = DistExecutor.safeRunForDist(()-> ClientProxy::new, ()->CommonProxy::new);
 
 	//public static final SimpleNetworkWrapper packetHandler = NetworkRegistry.INSTANCE.newSimpleChannel(MODID);
 
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent e) 
-    {
-        IEnContent.preInit();
-        proxy.preInit();
-        
-        
-        
-        IEnContent.preInitEnd();
-        proxy.preInitEnd();
-    }
 
+	public ImmersiveEnergy()
+	{
+		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, IEnServerConfig.ALL);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+	}
+
+	public void setup(FMLCommonSetupEvent e)
+    {
+		proxy.preInit();
+		IEnContent.preInit();
+		IEnPacketHandler.preInit();
+
+		IEnContent.preInitEnd();
+		proxy.preInitEnd();
+		//---------
+
+		IEnContent.init();
+
+		proxy.init();
+
+		IEnContent.initEnd();
+		proxy.initEnd();
+		//---------
+
+		MinecraftForge.EVENT_BUS.register(new RenderGuiHandler());
+		IEnContent.postInit();
+		proxy.postInit();
+
+
+
+/*
+		for(Item item : IEnContent.registeredIEnItems)
+		{
+			if(item instanceof IEnArrowBase)
+			{
+				BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(item, new BehaviorProjectileDispense()
+				{
+					@Override
+					protected IProjectile getProjectileEntity(World worldIn, IPosition position, ItemStack stackIn)
+					{
+						IEnArrowBase arrowItem = ((IEnArrowBase) item);
+						final EntityIEnArrow arrow = new EntityIEnArrow(worldIn, arrowItem.getDropItem().copy(), position.getX(), position.getY(), position.getZ()).setIgnoreInvulnerability(arrowItem.getIgnoreInvulnerability());
+						arrow.setLogic(arrowItem.getLogic());
+						arrow.setDamage(arrowItem.getArrowDamage());
+						arrow.setKnockbackStrength(arrowItem.getKnockback());
+						arrow.pickupStatus = EntityArrow.PickupStatus.ALLOWED;
+						if(arrowItem.isFlaming())
+						{
+							arrow.setFire(100);
+						}
+						return arrow;
+					}
+				});
+			}
+		}*/
+		IEnContent.postInitEnd();
+		proxy.postInitEnd();
+
+    }
+/*
     @Mod.EventHandler
     public void init(FMLInitializationEvent e) 
     {
@@ -71,61 +118,18 @@ public class ImmersiveEnergy
 
 		IEnContent.initEnd();
     	proxy.initEnd();
-    }
+    }*/
 
-    @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent e) 
-    {
-    	MinecraftForge.EVENT_BUS.register(new RenderGuiHandler());
-        IEnContent.postInit();
-        proxy.postInit();
-        
-        
-        
-        
-        for(Item item : IEnContent.registeredIEnItems)
-		{
-			if(item instanceof IEnArrowBase)
-			{
-				BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(item, new BehaviorProjectileDispense()
-				{
-					@Override
-					protected IProjectile getProjectileEntity(World worldIn, IPosition position, ItemStack stackIn)
-					{
-						IEnArrowBase arrowItem = ((IEnArrowBase) item);
-						final EntityIEnArrow arrow = new EntityIEnArrow(worldIn, arrowItem.getDropItem().copy(), position.getX(), position.getY(), position.getZ()).setIgnoreInvulnerability(arrowItem.getIgnoreInvulnerability());
-				        arrow.setLogic(arrowItem.getLogic());
-				        arrow.setDamage(arrowItem.getArrowDamage());
-				        arrow.setKnockbackStrength(arrowItem.getKnockback());
-				        arrow.pickupStatus = EntityArrow.PickupStatus.ALLOWED;
-				        if(arrowItem.isFlaming()) 
-				        {
-				            arrow.setFire(100);
-				        }
-				        return arrow;
-					}
-				});
-			}
-		}
-        IEnContent.postInitEnd();
-        proxy.postInitEnd();
-    }
-
-	@Mod.EventHandler
-	public void loadComplete(FMLLoadCompleteEvent event)
-	{
+	public void loadComplete(FMLLoadCompleteEvent event){
 		IEnCompatModule.doModulesLoadComplete();
 	}
 
 
-	public static CreativeTabs creativeTab = new CreativeTabs(MODID)
-	{
+	public void serverStarting(FMLServerStartingEvent event){
+		proxy.serverStarting();
+    }
 
-		@Override
-		public ItemStack getTabIconItem()
-		{
-			return new ItemStack(IEContent.blockMetalDecoration0, 1, 1);
-		}
-	};
+
+
 
 }
